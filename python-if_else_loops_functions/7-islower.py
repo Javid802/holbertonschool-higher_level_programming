016100ff49fd923b4d699139e33ceb2ca130d230
javid802 <gccavid123@gmail.com>
2026-03-16 16:10:52 +0400

    Cavid

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= "a" and c <= "z":
+    if len(c) != 1:
+        raise ValueError("Input must be a single character")
+    if 'a' <= c <= 'z':
         return True
+    elif 'A' <= c <= 'Z':
+        return None
     else:
         return None
-
