5ad3c34d768c3b1e33c7e61942a1c64bcb6f7366
javid802 <gccavid123@gmail.com>
2026-03-16 15:44:32 +0400

    Cavid

--- python-if_else_loops_functions/6-print_comb3 copy.py
+++ python-if_else_loops_functions/6-print_comb3 copy.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+siyahi = []
 for i in range(99):
     i = str(i)
     if len(str(i)) == 1:
         i = "0" + str(i)
+    siyahi.append(i)
     
-    if i[::-1] > i:
+    if i[::-1] not in siyahi:
         print("{}".format(i), end=", ")
