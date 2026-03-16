b49ac9fa2093c0a153deafa069f9a7b917a58050
javid802 <gccavid123@gmail.com>
2026-03-16 15:31:55 +0400

    Cavid

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
-for i in range(100):
+for i in range(99):
     if len(str(i))==1:
-        print("0{}".format(i), sep=",")
-    else:
-        print("{}".format(i), sep=",")
+        i = "0" + str(i)
+    print("{}".format(i), end=", ")
+print("99")
