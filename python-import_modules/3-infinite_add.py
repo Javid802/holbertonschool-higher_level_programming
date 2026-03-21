025e65b5a8439f6dd073c2b2e543cdb0ceb17a0f
javid802 <gccavid123@gmail.com>
2026-03-21 20:42:57 +0400

    Cavid

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ import sys
 
 len_argv = len(sys.argv)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     sum = 0
-    for i in range(1,len_argv):
-        sum+= int(sys.argv[i])
+    for i in range(1, len_argv):
+        sum += int(sys.argv[i])
     print(sum)
