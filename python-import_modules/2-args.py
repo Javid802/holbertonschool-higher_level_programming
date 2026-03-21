51c7f08a4b50de402835cf4e23816e9737121166
javid802 <gccavid123@gmail.com>
2026-03-21 20:36:44 +0400

    Cavid

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+sds
+len_argv = len(sys.argv)
+if __name__ == "__main__":
+    if len_argv < 2:
+        print("{} arguments.".format(0))
+    else:
+        if len_argv < 3:
+            print("{} argument:".format(len_argv - 1))
+        else:
+            print("{} arguments:".format(len_argv - 1))
+        for i in range(1, len_argv):
+            print("{}: {}".format(i, sys.argv[i]))
+adsasda
