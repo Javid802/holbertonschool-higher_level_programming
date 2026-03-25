d1ad6abcd3f95b26edb0bf9fa48f79a49d8dac51
javid802 <gccavid123@gmail.com>
2026-03-25 23:59:51 +0400

    Cavid

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
         soz = i
         key = a_dictionary.get(i)
-        print("{}: {}".format(soz,key))
+        print("{}: {}".format(soz, key))
