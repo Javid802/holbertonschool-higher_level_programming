3440cfdac5296ed410921dc588746e64539f7dca
javid802 <gccavid123@gmail.com>
2026-03-25 23:45:16 +0400

    Cavid

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     salam = []
-    for i, z in zip(set_1,set_2):
-        if i == z:
-           salam.append(i)
-    return(salam)
+    for i in set_1:
+        for z in set_2:
+            if i == z:
+                salam.append(i)
+    return salam
