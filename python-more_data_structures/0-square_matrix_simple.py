58d31460ce79b41f9000c49a1e34f0ebf16c6462
javid802 <gccavid123@gmail.com>
2026-03-25 23:13:08 +0400

    Cavid

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         row = []
         for z in range(len(matrix[i])):
-            row.append(matrix[i][z]**2)
+            row.append(matrix[i][z] ** 2)
         new_matrix.append(row)
     return new_matrix
-            
