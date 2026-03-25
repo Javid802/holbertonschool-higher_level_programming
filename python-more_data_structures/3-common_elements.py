84e0bce22896bfa4a00b472f2113dec9df6cbd66
javid802 <gccavid123@gmail.com>
2026-03-25 23:44:00 +0400

    Cavid

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -5,10 +5,3 @@ def common_elements(set_1, set_2):
         if i == z:
            salam.append(i)
     return(salam)
-
-
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
