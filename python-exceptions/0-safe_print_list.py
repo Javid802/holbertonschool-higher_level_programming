1b9238ba4525058e10bc4a8953fa46dfad2649a4
javid802 <gccavid123@gmail.com>
2026-03-28 01:59:44 +0400

    Cavid

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        a = ''
-        for i in range(x):
-            a = a + str(my_list[i])
-        return a
-    except IndexError:
-        print('salam')
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
