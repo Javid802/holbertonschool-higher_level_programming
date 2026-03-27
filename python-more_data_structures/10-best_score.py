809d62b8a067c31af0de2579ae7ba776b8b8974d
javid802 <gccavid123@gmail.com>
2026-03-28 01:12:25 +0400

    Cavid

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     a = 0
+    ad = ''
     for i in a_dictionary:
         if a_dictionary[i] > a:
             a = a_dictionary[i]
+            ad = i
     if a == 0:
         return None
     else:
-        return a
+        return ad
+
