e30e18d72d2feabc9bda01ce4bbebc0f298b0de2
javid802 <gccavid123@gmail.com>
2026-03-28 01:32:48 +0400

    Cavid

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    cem = 0
+    evvelki = 0
+    for i in reversed(roman_string):
+        deyer = roman[i]
+        if deyer >= evvelki:
+            total += deyer
+        else:
+            total -= deyer
+        evvelki = deyer
+    return cem
