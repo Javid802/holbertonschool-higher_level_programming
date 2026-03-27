dad304f575776c0299a9a188ead6ab55d597b335
javid802 <gccavid123@gmail.com>
2026-03-28 02:09:28 +0400

    Cavid

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             elements_printed += 1
-        except (IndexError, TypeError,ValueError):
+        except (IndexError, TypeError):
             # Xəta baş verdikdə dövrü dayandırırıq
             break
     print("")  # Yeni sətir
