a8c220ba82a94aece4ced6928ed1baec2dcf400b
javid802 <gccavid123@gmail.com>
2026-04-19 22:08:49 +0400

    Cavid

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that reads a UTF-8 text file and prints it"""
+"""Reads a text file"""
 
 
 def read_file(filename=""):
-    """Reads a file and prints its content to stdout"""
-    with open(filename, encoding="utf-8") as f:
+    """UTF-8 open"""
+    with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
